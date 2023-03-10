25acaa1b962e5516f2e6b91da0b427e2a7a8bb82
tili6 <123707038+tili6@users.noreply.github.com>
2023-03-09 17:00:57 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let button = document.querySelector("button");
+
+button.onclick = function() {
+    let answer = Number(document.querySelector(".answer").value);
+let answer1 = document.querySelector(".answer1").value;
+    console.log(answer); 
+    console.log(answer1);
+    if (answer > 0 || answer >90){console.log("yay");
+    }
+    
+};
